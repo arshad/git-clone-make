4f2d0053939fa15f17a4eb58e2c1e50cb07005c6
Arshad <arshad@chummun.com>
2014-03-04 02:43:02 +0400

    Parse .make file and make a list of projects

--- git-multi-clone.rb
+++ git-multi-clone.rb
@@ -1,11 +1,49 @@
 #!/usr/bin/env ruby
 
-repos = Hash[*File.read('restaurant.repo').split(/[, \n]+/)]
+projects = {}
+current_project_name = ''
 
-repos.each do |url, destination|
-  # Get the name of the repo to clone from url.
-  name = url.scan(/([\w\-]*).git$/).last.first
+File.open('restaurant.make') do |f|
+  f.each_line do |line|
 
-  # Call git clone.
-  system("git clone #{url} #{destination}/#{name}");
+    # Get the project name.
+    if match = line.match(/^projects\[([\w]*)\]/i)
+      name = match[1].to_s
+    end
+
+    # Determine if this is a new project.
+    if name && name != current_project_name
+      current_project_name = name
+      projects[current_project_name] = { :name => name, :type => '', :revision => '', :branch => ''}
+    end
+
+    # Get project download type.
+    if match = line.match(/^projects\[[\w]*\]\[download\]\[type\][\s]*=[\s]*(...)$/i)
+      projects[current_project_name][:type] = match[1]
+    end
+
+    # Get project revision.
+    if match = line.match(/^projects\[[\w]*\]\[download\]\[revision\][\s]*=[\s]*([\S]*)/i)
+      projects[current_project_name][:revision] = match[1]
+    end
+
+    # Get project branch.
+    if match = line.match(/^projects\[[\w]*\]\[download\]\[branch\][\s]*=[\s]*([\S]*)/i)
+      projects[current_project_name][:branch] = match[1]
+    end
+  end
 end
+
+projects.each do |name, project|
+  puts name
+end
+
+# puts projects
+
+# repos.each do |url, destination|
+#   # Get the name of the repo to clone from url.
+#   name = url.scan(/([\w\-]*).git$/).last.first
+
+#   # Call git clone.
+#   system("git clone #{url} #{destination}/#{name}");
+# end
