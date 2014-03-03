fff8ce281ff2c7ccf91e4cf26ef06e35bd095a75
Arshad <arshad@chummun.com>
2014-03-04 00:28:14 +0400

    Basic read from repo and clone working

--- git-multi-clone.rb
+++ git-multi-clone.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 
+repos = Hash[*File.read('restaurant.repo').split(/[, \n]+/)]
 
+repos.each do |url, destination|
+  # Get the name of the repo to clone from url.
+  name = url.scan(/([\w\-]*).git$/).last.first
 
+  # Call git clone.
+  system("git clone #{url} #{destination}/#{name}");
+end
